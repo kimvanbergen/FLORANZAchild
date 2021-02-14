03e239d217ac18f07d2bfb3ff2aeee278a43552e
Kim van Bergen <kim@kimbob.de>
2021-02-14 18:29:54 +0100

    setup commit

---
+++ Bootstrap.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Template\NOVAChild;
+
+/**
+ * Class Bootstrap
+ * @package Template\NOVAChild
+ */
+class Bootstrap extends \Template\NOVA\Bootstrap
+{
+
+}
